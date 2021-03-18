80b5718256f4061f2610f62878f488b3e14e3f07
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-18 14:17:24 +0100

    add first task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello'); //test
+// console.log('Hello'); //test
+
+/* 
+***TASK:
+1. Ask first name
+2. Ask last name
+3. Ask favorite color
+4. Merge information and add "21" to create password
+*/
+
+// .1
+var firstName = prompt('What\'s your name?');
+console.log(firstName); //test
+console.log(typeof (firstName)); //test
