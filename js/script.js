bbb24ccb42d0e2da36a23429d55a347b711befba
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-18 15:20:34 +0100

    add style.css

--- js/script.js
+++ js/script.js
@@ -11,23 +11,23 @@
 
 // .1
 var firstName = prompt('What\'s your first name?');
-console.log(firstName); //test
-console.log(typeof (firstName)); //test
+// console.log(firstName); //test
+// console.log(typeof (firstName)); //test
 
 // .2
 var lastName = prompt('What\'s your last name?');
-console.log(lastName); //test
-console.log(typeof (lastName)); //test
+// console.log(lastName); //test
+// console.log(typeof (lastName)); //test
 
 // .3
 var favoriteColor = prompt('What\'s your favorite color?');
-console.log(favoriteColor); //test
-console.log(typeof (favoriteColor)); //test
+// console.log(favoriteColor); //test
+// console.log(typeof (favoriteColor)); //test
 
 // .4
 var password = firstName + lastName + favoriteColor + '21';
-console.log(password); //test
-console.log(typeof (password)); //test
+// console.log(password); //test
+// console.log(typeof (password)); //test
 
 // .5
-document.getElementById('view-password').innerHTML = 'Your password is ' + password;
+document.getElementById('view-password').innerHTML = password;
