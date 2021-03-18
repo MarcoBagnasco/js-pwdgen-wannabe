637935c30fc4b201c51d3a38913ee9133236998f
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-18 14:22:24 +0100

    add fourth task

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 2. Ask last name
 3. Ask favorite color
 4. Merge information and add "21" to create password
+5. Show password
 */
 
 // .1
@@ -13,12 +14,17 @@ var firstName = prompt('What\'s your first name?');
 console.log(firstName); //test
 console.log(typeof (firstName)); //test
 
-//.2
+// .2
 var lastName = prompt('What\'s your last name?');
 console.log(lastName); //test
 console.log(typeof (lastName)); //test
 
-//.3
+// .3
 var favoriteColor = prompt('What\'s your favorite color?');
 console.log(favoriteColor); //test
 console.log(typeof (favoriteColor)); //test
+
+// .4
+var password = firstName + lastName + favoriteColor + '21';
+console.log(password); //test
+console.log(typeof (password)); //test
